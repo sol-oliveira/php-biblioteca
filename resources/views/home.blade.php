0346aa058a37a1b296a16ede2d3c48c57d944e8e
sole7 <sole-oliveira@hotmail.com>
2020-01-11 13:55:35 -0300

    ajuste home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,33 @@
+
 @extends('layout.site')
 
 @section('titulo','Biblioteca')
 
 @section('conteudo')
   <div class="container">
-    <h3 class="center">Lista de livros</h3>
-    <div class="row">
-      @foreach($livros as $livro)
-        <div class="col s12 m4">
-          <div class="card">
+    <h3 class="center"></h3>
+    <div class="row  ">
+        <div class="col s12 m4 ">
+          <div style="margin-top:100px" class="card deep-orange lighten-4">
             <div class="card-image" >
-              <img  src="{{asset($livro->imagem)}}">
-            </div>
-            <div class="card-content">
-              <h4>{{$livro->titulo}}</h4>
-              <p>{{$livro->quantidade}}</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Ver mais...</a>
-            </div>
+              <img height="560" src="{{ asset('img/imagem_4.jpg') }}">
+            </div>           
           </div>
-        </div>
-      @endforeach
+        </div> 
+        <div class="col s12 m4">
+          <div style="margin-top:100px" class="card deep-orange lighten-4 ">
+            <div class="card-image" >
+              <img height="560" src="{{ asset('img/imagem_2.jpeg') }}">
+            </div>          
+          </div>
+        </div> 
+        <div class="col s12 m4">
+          <div style="margin-top:100px" class="card deep-orange lighten-4">
+            <div class="card-image" >
+              <img  height="560"  src="{{ asset('img/imagem_3.jpeg') }}">
+            </div>           
+          </div>
+        </div>    
     </div>
   </div>
 
